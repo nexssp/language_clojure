586fff1c10995bc060756fae469d68623346c160
Marcin Polak <mapoart@gmail.com>
2020-07-22 04:26:56 +0200

    Adjusted templates with :let

--- templates/default.clj
+++ templates/default.clj
@@ -1,10 +1,10 @@
-
 ; Nexss PROGRAMMER 2.x - Clojure
 ; Default template for JSON Data
 ; profiles.clj must be copied first to your ~/.lein/profiles.clj
 (require '[jsonista.core :as j])
-(doseq [ln (line-seq (java.io.BufferedReader. *in*))]
-   (def parsed-json (j/read-value ln))
+(doseq [ln (line-seq (java.io.BufferedReader. *in*))
+         :let [parsed-json (j/read-value ln)]
+       ]
    (def nexss-stdout (update parsed-json :test #(str "test" %)))
    (println (j/write-value-as-string nexss-stdout))
 )
--- templates/helloWorld.clj
+++ templates/helloWorld.clj
@@ -3,8 +3,9 @@
 ; Default template for JSON Data
 ; profiles.clj must be copied first to your ~/.lein/profiles.clj
 (require '[jsonista.core :as j])
-(doseq [ln (line-seq (java.io.BufferedReader. *in*))]
-   (def parsed-json (j/read-value ln))
+(doseq [ln (line-seq (java.io.BufferedReader. *in*))
+         :let [parsed-json (j/read-value ln)]
+       ]
    (def nexss-stdout (assoc parsed-json :helloFromClojure (clojure-version)))
    (println (j/write-value-as-string nexss-stdout))
 )
